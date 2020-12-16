de2bcdf4094db6a4476ddf6bfaeab2b34caf0f61
hm <mark.huang1212@gmail.com>
2020-12-16 22:35:31 +0800

    progress

--- src/redis/RedisResponseParser.ts
+++ src/redis/RedisResponseParser.ts
@@ -1,16 +1,61 @@
+import events from 'events'
+
 enum RedisResponseParsingStage {
-    ParseNotBegin,
     ParseType,
     ParseLength,
     ParseContent,
     ParseFinished
 }
 
-class RedisResponseParser {
+enum RedisResponseType {
+    SimpleString = '+',
+    Error = '-',
+    Integer = ':',
+    BulkString = '$',
+    Array = '*'
+}
 
-    stage = RedisResponseParsingStage.ParseNotBegin
+function isRedisResponseType(str: string) {
+    return str == '+' || str == '-' || str == ':' || str == '$' || str == '*'
+}
+
+class RedisResponseParser extends events.EventEmitter {
+
+    stage = RedisResponseParsingStage.ParseType
+
+    type?: RedisResponseType = undefined
+
+    length?: number = 0
+
+    isSimpleContent?: boolean = undefined
+
+    temp?: Buffer = undefined
 
     write(str: Buffer) {
+        let atByte = 0
+        while (atByte < str.length) {
 
+            if (this.stage == RedisResponseParsingStage.ParseType) {
+                this.stage = RedisResponseParsingStage.ParseType
+                const firstByte = str.slice(0, 1).toString('ascii')
+                if (!isRedisResponseType(firstByte))
+                    this.emit('error', Error('Invalid Response Type'))
+                this.type = firstByte as RedisResponseType
+                atByte += 1
+            }
+
+            if (this.stage == RedisResponseParsingStage.ParseLength) {
+                this.stage = RedisResponseParsingStage.ParseContent
+                if (this.type == RedisResponseType.BulkString) {
+                    
+                }
+                if (this.type == RedisResponseType.Array) {
+
+                }
+                
+            }
+        }
     }
 }
+
+export { RedisResponseParser }
--- src/redis/SimpleRedisClient.spec.ts
+++
@@ -1,8 +0,0 @@
-import SimpleRedisClient from "./SimpleRedisClient"
-
-describe('Simple Redis Client', () => {
-    it('Simple Cmd', async () => {
-        const client = new SimpleRedisClient()
-        console.log(await client.smallCmd(SimpleRedisClient.makeCmd('set hello world')))
-    })
-})
