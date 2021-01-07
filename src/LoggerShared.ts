aa580c062571cb8eced9c9d0090d9a6de88b545f
hm <mark.huang1212@gmail.com>
2021-01-07 17:20:27 +0800

    Create folder for logging if necessary

--- src/LoggerShared.ts
+++ src/LoggerShared.ts
@@ -1,10 +1,14 @@
 import pino from 'pino'
 import path from 'path'
 import dayjs from 'dayjs'
+import fs from 'fs'
 
 const time = dayjs().unix()
 
 const logMode = process.env.NODE_ENV === "development" ? 'development' : 'production'
+
+if (fs.existsSync(path.join(__dirname, '../logs')) === false)
+    fs.mkdirSync(path.join(__dirname, '../logs'))
 
 const LoggerShared = pino({
     level: logMode === 'development' ? 'trace' : 'info'
